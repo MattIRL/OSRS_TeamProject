4d2488dfed9ee869694fced809d1a75c1d983e7a
MattIRL <mlewis.tc@gmail.com>
2025-10-16 16:34:37 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OSRS_TeamProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
